824f8baacfc1586820e7ec28b7eb313ee8689047
raph <raph@e-frogg.com>
2016-02-02 21:46:02 +0100

    structure de base HTML

---
+++ dynamic/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raphaelmiw
+ * Date: 02/02/16
+ * Time: 21:02
+ */
+echo "coucou"
+
+
+?>
+    </body>
+</html>
